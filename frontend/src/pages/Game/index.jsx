f7af2ed767a046863a0f19cbf170c29254c95e2b
Quang Hoang <hoangquang2508@gmail.com>
2025-02-04 19:52:29 +0700

    Update index.jsx

--- frontend/src/pages/Game/index.jsx
+++ frontend/src/pages/Game/index.jsx
@@ -5,7 +5,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import io from "socket.io-client";
 import axios from "axios";
 
-const socket = io(process.env.REACT_APP_API_URL);
+const socket = io.connect(process.env.REACT_APP_API_URL);
 
 const Game = ({ mode }) => {
   const navigate = useNavigate();
